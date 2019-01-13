c3e044e0df0bb1bb3f7d3cb6c9dd22fb7dff3898
alex <truesea@gmail.com>
2019-01-13 22:24:14 +0800

    类

---
+++ corejava/src/main/java/me/sanio/study/javalang/classandobject/Sdate.java
@@ -0,0 +1,11 @@
+package me.sanio.study.javalang.classandobject;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Sdate {
+    public static void main(String[] args){
+        System.out.println(new Date());
+        System.out.println(LocalDate.now());
+    }
+}
