8698dda552edb525bef5eb126da36eff801e54c7
alex <truesea@gmail.com>
2019-01-13 23:41:44 +0800

    update

--- corejava/src/main/java/me/sanio/study/javalang/classandobject/CalendarTest.java
+++ corejava/src/main/java/me/sanio/study/javalang/classandobject/CalendarTest.java
@@ -3,27 +3,24 @@ package me.sanio.study.javalang.classandobject;
 import java.time.*;
 
 /**
- * @version 1.5 2015-05-08
  * @author Cay Horstmann
+ * @version 1.5 2015-05-08
  */
 
-public class CalendarTest
-{
-    public static void main(String[] args)
-    {
+public class CalendarTest {
+    public static void main(String[] args) {
         LocalDate date = LocalDate.now();
         int month = date.getMonthValue();
         int today = date.getDayOfMonth();
 
-        date = date.minusDays(today ); // Set to start of month
+        date = date.minusDays(today - 1); // Set to start of month
         DayOfWeek weekday = date.getDayOfWeek();
         int value = weekday.getValue(); // 1 = Monday, ... 7 = Sunday
 
         System.out.println("Mon Tue Wed Thu Fri Sat Sun");
         for (int i = 1; i < value; i++)
             System.out.print("    ");
-        while (date.getMonthValue() == month)
-        {
+        while (date.getMonthValue() == month) {
             System.out.printf("%3d", date.getDayOfMonth());
             if (date.getDayOfMonth() == today)
                 System.out.print("*");
