41e9fd8d39324b2d5ab93ee5d74a10a0fcd9b08b
alex <392661092@qq.com>
2019-06-19 10:01:58 +0800

    1

--- corejava/src/main/java/me/sanio/study/javalang/datatype/All8Types.java
+++ corejava/src/main/java/me/sanio/study/javalang/datatype/All8Types.java
@@ -1,6 +1,9 @@
 package me.sanio.study.javalang.datatype;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class All8Types {
     public static void main(String[] args) {
@@ -16,6 +19,22 @@ public class All8Types {
         System.out.println(a.subtract(b));
         System.out.println(15/2);
         System.out.println(15.0/2);
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDateTime.now());
+        System.out.println(LocalTime.now());
+        for (int i = 0; i <100 ; i++) {
+            //System.out.println(System.currentTimeMillis());
+            //System.out.println(getmicTime());
+            System.out.println(System.nanoTime());
+        }
+
+
+    }
+
+    public static Long getmicTime() {
+        Long cutime = System.currentTimeMillis() * 1000; // 微秒
+        Long nanoTime = System.nanoTime(); // 纳秒
+        return cutime + (nanoTime - nanoTime / 1000000 * 1000000) / 1000;
     }
 
 }
