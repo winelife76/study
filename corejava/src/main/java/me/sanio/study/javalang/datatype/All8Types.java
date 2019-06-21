b3f6265f999a652b60c88f3d4eae64b2ed922cfd
alex <truesea@gmail.com>
2019-06-21 19:30:06 +0800

    update

--- corejava/src/main/java/me/sanio/study/javalang/datatype/All8Types.java
+++ corejava/src/main/java/me/sanio/study/javalang/datatype/All8Types.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class All8Types {
     public static void main(String[] args) {
@@ -11,6 +12,7 @@ public class All8Types {
         int intT = 12598;
         long longT = 65530;
         byte byteT = 0b1001;//,0b,0B->二进制9
+        ArrayList<Integer> nanotime = new ArrayList<>();
 
         BigDecimal a = BigDecimal.valueOf(2.0);
         BigDecimal b = BigDecimal.valueOf(1.1);
@@ -25,7 +27,8 @@ public class All8Types {
         for (int i = 0; i <100 ; i++) {
             //System.out.println(System.currentTimeMillis());
             //System.out.println(getmicTime());
-            System.out.println(System.nanoTime());
+            nanotime.add(i);
+            System.out.println(System.nanoTime()+":"+nanotime.size());
         }
 
 
