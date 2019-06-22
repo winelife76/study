73ac7872274852bb7606032b1ad38fa615244853
alex <truesea@gmail.com>
2019-06-22 12:39:31 +0800

    update

--- corejava/src/main/java/me/sanio/study/javalang/classandobject/Employee.java
+++ corejava/src/main/java/me/sanio/study/javalang/classandobject/Employee.java
@@ -3,13 +3,14 @@ package me.sanio.study.javalang.classandobject;
 import me.sanio.study.javalang.inheritance.Person;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Employee extends Person {
     private double salary;
     private LocalDate hireDay;
 
-    public  Employee(){
-        this("",100.0,2019,6,20);
+    public Employee() {
+        this("", 100.0, 2019, 6, 20);
     }
 
     public Employee(String n, double s, int year, int month, int day) {
@@ -31,11 +32,29 @@ public class Employee extends Person {
         salary += raise;
     }
 
-    public Employee getBuddy(){
+    public Employee getBuddy() {
         return null;
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return String.format("an employee with a salary of $%.2f", salary);
     }
+
+   /* @Override
+    public boolean equals(Object obj) {
+        // a quick test to see if the objects are identical
+        if (this == obj) return true;
+
+        // must return false if the explicit parameter is null
+        if (obj == null) return false;
+
+        // if the classes don't match, they can't be equal
+        if (getClass() != obj.getClass()) return false;
+
+        // now we know otherObject is a non-null Employee
+        Employee other = (Employee) obj;
+
+        // test whether the fields have identical values
+        return Objects.equals(super.getName(), other.getName()) && salary == other.salary && Objects.equals(hireDay, other.hireDay);
+    }*/
 }
--- corejava/src/main/java/me/sanio/study/javalang/inheritance/Person.java
+++ corejava/src/main/java/me/sanio/study/javalang/inheritance/Person.java
@@ -12,4 +12,5 @@ public abstract class  Person {
     }
 
     public abstract String getDescription();
+
 }
