2bc274fc81778576e9adf7b935e71e224be3a7fe
alex <392661092@qq.com>
2019-07-29 10:43:42 +0800

    重新组织结构

--- corejava/src/main/java/me/sanio/study/javalang/function/BinaryOperator.java
+++
@@ -1,4 +0,0 @@
-package me.sanio.study.javalang.function;
-
-public interface BinaryOperator extends Function<Integer,Function<Integer,Integer>> {
-}
--- corejava/src/main/java/me/sanio/study/javalang/function/Function.java
+++
@@ -1,5 +0,0 @@
-package me.sanio.study.javalang.function;
-
-public interface Function<T, U> {
-    U apply(T arg);
-}
